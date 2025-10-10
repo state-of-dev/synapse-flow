ed92bdf23d5d0eaf21c5ae9f2c23e30d763546b8
fg-dev-ebc <lf.fernandez002@ebc.edu.mx>
2025-10-09 21:46:08 -0600

    boton context eliminado

--- components/multimodal-input.tsx
+++ components/multimodal-input.tsx
@@ -325,8 +325,7 @@ function PureMultimodalInput({
             ref={textareaRef}
             rows={1}
             value={input}
-          />{" "}
-          <Context {...contextProps} />
+          />
         </div>
         <PromptInputToolbar className="!border-top-0 border-t-0! p-0 shadow-none dark:border-0 dark:border-transparent!">
           <PromptInputTools className="gap-0 sm:gap-0.5">
